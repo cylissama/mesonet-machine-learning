66d76e94a93e777f6a239c4b861c377fda7990d2
Cy Dixon <94246986+cylissama@users.noreply.github.com>
2025-02-27 13:14:35 -0600

    code update

--- PRISM_codes/convert_bil_to_csv.py
+++ PRISM_codes/convert_bil_to_csv.py
@@ -5,9 +5,10 @@ import numpy as np
 import pandas as pd
 
 # Configuration
-BIL_FILE = "/Volumes/Mesonet/spring_ml/PRISM_data/prism_2020_ppt_data/PRISM_ppt_stable_4kmM3_2020_bil.bil"  
+BIL_FILE = "/Volumes/Mesonet/spring_ml/PRISM_data/an/ppt/daily/2019/prism_ppt_us_30s_20190101.bil"  
+# /Volumes/Mesonet/spring_ml/PRISM_data/4km/ppt/2020/PRISM_ppt_stable_4kmM3_2020_all_bil/PRISM_ppt_stable_4kmM3_2020_bil.bil
 # Your input file
-CSV_OUTPUT = "precipitation_2020.csv"                  # Output CSV
+CSV_OUTPUT = "prism_ppt_us_30s_20190101.csv"                  # Output CSV
 SUBSAMPLE = 10  # Process every Nth pixel to reduce file size (set to 1 for all data)
 
 # Read BIL file
